4aacf4d0cda17849ce71239a90ed1d3089dd753b
Daniel C Nwaeke <65520086+danzisky@users.noreply.github.com>
2023-07-06 21:59:05 +0300

    created the models controlers and migrations

---
+++ app/Http/Controllers/IntervalController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Interval;
+use Illuminate\Http\Request;
+
+class IntervalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Interval $interval)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Interval $interval)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Interval $interval)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Interval $interval)
+    {
+        //
+    }
+}
